9ff1380db325fb6ff127c1b4acf2a74bf7493a0f
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-02-13 23:04:33 +1300

    Add series and parallel tests

--- unit_tests/test_circuits.py
+++ unit_tests/test_circuits.py
@@ -1,4 +1,4 @@
-from msignal.mcircuit import *
+from mcircuit import *
 import unittest
 import sympy as sym
 from msignal.mrf import MRF
@@ -37,13 +37,101 @@ class McircuitsTester(unittest.TestCase):
         self.assertEqual(a.V, 5 / s, "V incorrect.")
 
 
-    def test_RplusL(self):
+    def test_R_series_R(self):
+        """Mcircuits: check R + R
+
+        """
+        a = R(10) + R(5)
+        b = a.simplify()
+        self.assertEqual(b.R, 15, "R incorrect.")
+        self.assertEqual(b.Z, 15, "Z incorrect.")
+        self.assertEqual(type(b), R, "type incorrect.")
+
+
+    def test_L_series_L(self):
+        """Mcircuits: check L + L
+
+        """
+        a = L(10) + L(5)
+        b = a.simplify()
+        self.assertEqual(b.L, 15, "L incorrect.")
+        self.assertEqual(b.Z, 15 * s, "Z incorrect.")
+        self.assertEqual(type(b), L, "type incorrect.")
+
+
+    def test_C_series_C(self):
+        """Mcircuits: check C + C
+
+        """
+        a = C(10) + C(15)
+        b = a.simplify()
+        self.assertEqual(b.C, 6, "C incorrect.")
+        self.assertEqual(b.Z, 1 / (6 * s), "Z incorrect.")
+        self.assertEqual(type(b), C, "type incorrect.")
+
+
+    def test_V_series_V(self):
+        """Mcircuits: check V + V
+
+        """
+        a = V(10) + V(5)
+        b = a.simplify()
+        self.assertEqual(b.v, 15, "V incorrect.")
+        self.assertEqual(b.V, 15 / s, "V incorrect.")
+        self.assertEqual(type(b), V, "type incorrect.")
+
+
+    def test_R_series_L(self):
         """Mcircuits: check R + L
 
         """
         a = R(10) + L(5)
         self.assertEqual(a.Z, 5 * s + 10, "Z incorrect.")
         self.assertEqual(type(a), Thevenin, "type incorrect.")
+
+
+    def test_R_parallel_R(self):
+        """Mcircuits: check R | R
+
+        """
+        a = R(10) | R(15)
+        b = a.simplify()
+        self.assertEqual(b.R, 6, "R incorrect.")
+        self.assertEqual(b.Z, 6, "Z incorrect.")
+        self.assertEqual(type(b), R, "type incorrect.")
+
+
+    def test_L_parallel_L(self):
+        """Mcircuits: check L | L
+
+        """
+        a = L(10) | L(15)
+        b = a.simplify()
+        self.assertEqual(b.L, 6, "L incorrect.")
+        self.assertEqual(b.Z, 6 * s, "Z incorrect.")
+        self.assertEqual(type(b), L, "type incorrect.")
+
+
+    def test_C_parallel_C(self):
+        """Mcircuits: check C | C
+
+        """
+        a = C(10) | C(15)
+        b = a.simplify()
+        self.assertEqual(b.C, 25, "C incorrect.")
+        self.assertEqual(b.Z, 1 / (25 * s), "Z incorrect.")
+        self.assertEqual(type(b), C, "type incorrect.")
+
+
+    def test_I_parallel_I(self):
+        """Mcircuits: check I | I
+
+        """
+        a = I(10) | I(5)
+        b = a.simplify()
+        self.assertEqual(b.i, 15, "I incorrect.")
+        self.assertEqual(b.I, 15 / s, "I incorrect.")
+        self.assertEqual(type(b), I, "type incorrect.")
 
 
     def test_Shunt(self):
@@ -183,26 +271,26 @@ class McircuitsTester(unittest.TestCase):
         self.assertEqual(b.Z, 7.5, "Shunt loaded R incorrect Z.")
         
 
-    def test_opencircuit(self):
-        """Mcircuits: check opencircuit
+    def test_open_circuit(self):
+        """Mcircuits: check open_circuit
 
         """
 
         a = Shunt(R(10) + V(5))
-        b = a.opencircuit()
+        b = a.open_circuit()
 
         self.assertEqual(type(b), Thevenin, "type incorrect.")
         self.assertEqual(b.Z, 10, "incorrect Z.")
         self.assertEqual(b.V, 5 / s, "incorrect V.")
 
 
-    def test_shortcircuit(self):
-        """Mcircuits: check shortcircuit
+    def test_short_circuit(self):
+        """Mcircuits: check short_circuit
 
         """
 
         a = Series(R(10) + V(5))
-        b = a.shortcircuit()
+        b = a.short_circuit()
 
         self.assertEqual(type(b), Norton, "type incorrect.")
         self.assertEqual(b.Z, 10, "incorrect Z.")
