6f1bed5e7f416a5158f42132641ffa2f24c7b8a1
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-02-24 10:17:17 +1300

    Check Python version

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
+"""
+Mcircuit is a Python library for linear circuit analysis.  It uses SymPy
+for symbolic mathematics.
+"""
+
+#from __future__ import absolute_import, print_function
+from __future__ import print_function
+
+__version__ = "0.1-git"
+
+import sys
+if sys.version_info[0] == 2 and sys.version_info[1] < 6:
+    raise ImportError("Python Version 2.6 or above is required for Mcircuit.")
+else:  # Python 3
+    pass
+    # Here we can also check for specific Python 3 versions, if needed
+
+del sys
+
 from mcircuit import *
