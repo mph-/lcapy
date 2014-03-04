b78a9bb33f69e3e388bbe3dba9ab5943c37db4e8
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-03-04 21:11:42 +1300

    Tweak docs

--- demo/esd1.py
+++ demo/esd1.py
@@ -2,9 +2,14 @@ from mcircuit import V, R, L, C
 import numpy as np
 from matplotlib.pyplot import figure, savefig, show
 
-Vbody = 5e3
+# Human body model from MIL-STD-883, Method 3015.8, 
 Cbody = 100e-12
 Rbody = 1.5e3
+
+# Voltage on body.
+Vbody = 5e3
+
+# Device input capacitance.
 Cdev = 5e-12
 
 
@@ -15,7 +20,6 @@ t = np.linspace(0, 50e-9, 1000)
 
 fig = figure()
 ax = fig.add_subplot(111)
-# Voltage across R
 ax.plot(t * 1e9, b1.V.impulse_response(t) / 1e3, linewidth=2)
 ax.legend()
 ax.set_xlabel('Time (ns)')
