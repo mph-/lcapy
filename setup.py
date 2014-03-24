ff9d693142a25b9233553412b3411546565adcce
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-03-24 21:53:29 +1300

    Tweak version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = 'mcircuit',
-    version = '0.01',
+    version = '0.1.0',
     description = 'Linear circuit analysis',
     author = 'Michael Hayes',
     requires = [ 'sympy', 'numpy' ],
