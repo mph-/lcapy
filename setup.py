d301d9cf6388686c2ad032baec5eb143e0800c1d
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-03-25 22:42:37 +1300

    Tweak modules

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name = 'mcircuit',
-    version = '0.1.0zs',
+    version = '0.2.0',
     description = 'Linear circuit analysis',
     author = 'Michael Hayes',
     requires = [ 'sympy', 'numpy' ],
     author_email = 'michael.hayes@canterbury.ac.nz',
     url = 'https://github.com/mph-/mcircuit',
     download_url = 'https://github.com/mph-/mcircuit',
-    py_modules = [ 'mcircuit' ],
+    py_modules = [ 'mcircuit.mcircuit', 'mcircuit.netlist' ],
     scripts = [ ],
     classifiers=[
         'Development Status :: 4 - Beta',
