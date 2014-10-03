852994810bbb28901a7ecd6e58aeb60f997d6bb8
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-10-04 11:12:07 +1300

    Catch exception

--- lcapy/netlist.py
+++ lcapy/netlist.py
@@ -648,30 +648,35 @@ class Netlist(object):
 
     def Amatrix(self, n1, n2, n3, n4):
 
-        self.net_add('V1_', n1, n2)
-        
-        # A11 = V1 / V2 with I2 = 0
-        # Apply V1 and measure V2 with port 2 open-circuit
-        A11 = Avs(self.Vd['V1_'] / self.Voc(n3, n4))
-        
-        # A12 = V1 / I2 with V2 = 0
-        # Apply V1 and measure I2 with port 2 short-circuit
-        A12 = Zs(self.Vd['V1_'] / self.Isc(n3, n4))
+        try:
 
-        self.remove('V1_')
-        
-        self.net_add('I1_', n1, n2)
+            self.net_add('V1_', n1, n2)
+            
+            # A11 = V1 / V2 with I2 = 0
+            # Apply V1 and measure V2 with port 2 open-circuit
+            A11 = Avs(self.Vd['V1_'] / self.Voc(n3, n4))
+            
+            # A12 = V1 / I2 with V2 = 0
+            # Apply V1 and measure I2 with port 2 short-circuit
+            A12 = Zs(self.Vd['V1_'] / self.Isc(n3, n4))
+            
+            self.remove('V1_')
+            
+            self.net_add('I1_', n1, n2)
+            
+            # A21 = I1 / V2 with I2 = 0
+            # Apply I1 and measure I2 with port 2 open-circuit
+            A21 = Ys(-self.I['I1_'] / self.Voc(n3, n4))
+            
+            # A22 = I1 / I2 with V2 = 0
+            # Apply I1 and measure I2 with port 2 short-circuit
+            A22 = Ais(-self.I['I1_'] / self.Isc(n3, n4))
+            
+            self.remove('I1_')
+            return AMatrix(A11, A12, A21, A22)
 
-        # A21 = I1 / V2 with I2 = 0
-        # Apply I1 and measure I2 with port 2 open-circuit
-        A21 = Ys(-self.I['I1_'] / self.Voc(n3, n4))
-
-        # A22 = I1 / I2 with V2 = 0
-        # Apply I1 and measure I2 with port 2 short-circuit
-        A22 = Ais(-self.I['I1_'] / self.Isc(n3, n4))
-
-        self.remove('I1_')
-        return AMatrix(A11, A12, A21, A22)
+        except ValueError:
+            raise ValueError('Cannot create A matrix')
 
 
 class Circuit(Netlist):
