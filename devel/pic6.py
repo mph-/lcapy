a63fc22e23978d1236187d5958b09ca730c0b2d1
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-11-04 21:23:02 +1300

    Add explicit wire example

---
+++ devel/pic6.py
@@ -0,0 +1,15 @@
+from schematic import Schematic
+
+sch = Schematic()
+
+sch.net_add('P1 1 0.1')
+sch.net_add('R1 3 1; right')
+sch.net_add('L1 2 3; right')
+sch.net_add('C1 3 4; up')
+sch.net_add('L2 4 0; up')
+sch.net_add('P2 2 0.2')
+sch.net_add('W1 0 0.1; right')
+sch.net_add('W2 0.2 0; right')
+
+sch.draw()
+
