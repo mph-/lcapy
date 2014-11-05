18e7a5990531fa7b5e7e28c2bbe93f28c696a69b
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-11-06 08:44:34 +1300

    Add schematic examples

---
+++ demo/schematic/pic1.py
@@ -0,0 +1,12 @@
+from lcapy import Schematic
+
+sch = Schematic()
+
+sch.add('P1 1 0.1; v=V_1')
+sch.add('R1 3 1; right')
+sch.add('L1 2 3; right')
+sch.add('C1 3 0; down')
+sch.add('P2 2 0.2; v^=V_2')
+
+sch.draw()
+
