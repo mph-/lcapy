bfa6697ec466473c67ccfcfe942c202f9a6050cf
Michael Hayes <michael.hayes@canterbury.ac.nz>
2014-11-08 18:57:17 +1300

    Install schtex.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name = 'lcapy',
     url = 'https://github.com/mph-/lcapy',
     download_url = 'https://github.com/mph-/lcapy',
       py_modules = [ 'lcapy.core', 'lcapy.netlist', 'lcapy.oneport', 'lcapy.twoport', 'lcapy.threeport', 'lcapy.schematic'],
-    scripts = [ ],
+    scripts = ['scripts/schtex.py' ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
