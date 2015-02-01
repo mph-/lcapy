4ad6bf70cc160b49dbf30e4fa52a3db7bf619b19
Michael Hayes <michael.hayes@canterbury.ac.nz>
2015-02-01 17:48:08 +1300

    Compare canonical forms

--- unit_tests/test_circuits.py
+++ unit_tests/test_circuits.py
@@ -10,6 +10,11 @@ class LcapyTester(unittest.TestCase):
 
     """
 
+    def assertEqual2(self, ans1, ans2, comment):
+
+        self.assertEqual(ans1.canonical(), ans2.canonical(), comment)
+
+
     def test_RL1(self):
         """Lcapy: check RL network
 
@@ -18,13 +23,13 @@ class LcapyTester(unittest.TestCase):
         a.add('R1 1 2')
         a.add('C1 2 0')
 
-        self.assertEqual(a.Z(1, 2), R('R1').Z, "Z incorrect for R1.")
-        self.assertEqual(a.Z(2, 0), C('C1').Z, "Z incorrect for C1.")
-        self.assertEqual(a.Z(1, 0), (R('R1') + C('C1')).Z, "Z incorrect for R1 + C1.")
+        self.assertEqual2(a.Z(1, 2), R('R1').Z, "Z incorrect for R1.")
+        self.assertEqual2(a.Z(2, 0), C('C1').Z, "Z incorrect for C1.")
+        self.assertEqual2(a.Z(1, 0), (R('R1') + C('C1')).Z, "Z incorrect for R1 + C1.")
 
-        self.assertEqual(a.Y(1, 2), R('R1').Y, "Y incorrect for R1.")
-        self.assertEqual(a.Y(2, 0), C('C1').Y, "Y incorrect for C1.")
-        self.assertEqual(a.Y(1, 0).canonical(), (R('R1') + C('C1')).Y.canonical(), "Y incorrect for R1 + C1.")
+        self.assertEqual2(a.Y(1, 2), R('R1').Y, "Y incorrect for R1.")
+        self.assertEqual2(a.Y(2, 0), C('C1').Y, "Y incorrect for C1.")
+        self.assertEqual2(a.Y(1, 0), (R('R1') + C('C1')).Y, "Y incorrect for R1 + C1.")
 
 
     def test_VRL1(self):
@@ -37,11 +42,11 @@ class LcapyTester(unittest.TestCase):
         a.add('C1 2 0')
 
         # Note, V1 acts as a short-circuit for the impedance/admittanc
-        self.assertEqual(a.Z(1, 2).canonical(), (R('R1') | C('C1')).Z.canonical(), "Z incorrect across R1")
-        self.assertEqual(a.Z(2, 0).canonical(), (R('R1') | C('C1')).Z.canonical(), "Z incorrect across C1")
-        self.assertEqual(a.Z(1, 0).canonical(), R(0).Z.canonical(), "Z incorrect across V1")
+        self.assertEqual2(a.Z(1, 2), (R('R1') | C('C1')).Z, "Z incorrect across R1")
+        self.assertEqual2(a.Z(2, 0), (R('R1') | C('C1')).Z, "Z incorrect across C1")
+        self.assertEqual2(a.Z(1, 0), R(0).Z, "Z incorrect across V1")
 
-        self.assertEqual(a.Y(1, 2).canonical(), (R('R1') | C('C1')).Y.canonical(), "Y incorrect across R1")
-        self.assertEqual(a.Y(2, 0).canonical(), (R('R1') | C('C1')).Y.canonical(), "Y incorrect across C1")
+        self.assertEqual2(a.Y(1, 2), (R('R1') | C('C1')).Y, "Y incorrect across R1")
+        self.assertEqual2(a.Y(2, 0), (R('R1') | C('C1')).Y, "Y incorrect across C1")
         # This has a non-invertible A matrix.
-        # self.assertEqual(a.Y(1, 0).canonical(), R(0).Y.canonical(), "Y incorrect across V1")
+        # self.assertEqual2(a.Y(1, 0), R(0).Y, "Y incorrect across V1")
