3ce71a14e4ed713f788f9e554647270a076d968d
Michael Hayes <michael.hayes@canterbury.ac.nz>
2015-02-12 22:06:11 +1300

    Move unit tests

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@
 
 from distutils.core import setup
 
-setup(name = 'lcapy',
-    version = '0.6.4-git',
-    description = 'Symbolic linear circuit analysis',
-    author = 'Michael Hayes',
-    requires = [ 'sympy', 'numpy' ],
-    author_email = 'michael.hayes@canterbury.ac.nz',
-    url = 'https://github.com/mph-/lcapy',
-    download_url = 'https://github.com/mph-/lcapy',
-    py_modules = [ 'lcapy.core', 'lcapy.netlist', 'lcapy.oneport', 'lcapy.twoport', 'lcapy.threeport', 'lcapy.schematic', 'lcapy.mna', 'lcapy.plot'],
-    scripts = ['scripts/schtex.py'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        ]
-    )
+setup(name='lcapy',
+      version='0.6.4-git',
+      description='Symbolic linear circuit analysis',
+      author='Michael Hayes',
+      requires=['sympy', 'numpy'],
+      author_email='michael.hayes@canterbury.ac.nz',
+      url='https://github.com/mph-/lcapy',
+      download_url='https://github.com/mph-/lcapy',
+      py_modules=['lcapy.core', 'lcapy.netlist', 'lcapy.oneport', 'lcapy.twoport', 'lcapy.threeport', 'lcapy.schematic', 'lcapy.mna', 'lcapy.plot'],
+      scripts=['scripts/schtex.py'],
+      license='LGPL',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ]
+  )
