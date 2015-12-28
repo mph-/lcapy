8e0ef756f79299902085fef2022203cc5edc37f9
Michael Hayes <michael.hayes@canterbury.ac.nz>
2015-12-28 17:34:29 +1300

    Add parser

---
+++ lcapy/parser_test1.py
@@ -0,0 +1,46 @@
+from parser import parse
+
+r = parse('R1 2 3 4')
+print(r)
+
+r1 = parse('R1 2 3')
+print(r1)
+
+q = parse('Q1 2 3 4 pnp')
+print(q)
+
+j = parse('Ja2 2 3 4 pjf')
+print(j)
+
+s = parse('SW1 2 3 push')
+print(s)
+
+d1 = parse('D1 2 3 led')
+print(d1)
+
+v1 = parse('V1 2 3')
+print(v1)
+
+v1 = parse('V1 2 3 dc=4')
+print(v1)
+
+v2 = parse('V1 2 3 ac 10, 20')
+print(v2)
+
+v3 = parse('V1 2 3 sin(0 10 1000)')
+print(v3)
+
+v4 = parse('V1 2 3 1.0e3')
+print(v4)
+
+v5 = parse('V1 2 3 1e3')
+print(v5)
+
+r2 = parse('R1 2 3 {5 * a}')
+print(r2)
+
+i3 = parse('I1 2 3 sin(0 10 1000)')
+print(i3)
+
+e1 = parse('E1 2 3 4 5 1e3')
+print(e1)
