fcc5a8c6c6c6fefe203da2058a108ec09e828fe0
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-01-25 11:25:15 +1300

    Do not split options if comman in braces

--- lcapy/schematic.py
+++ lcapy/schematic.py
@@ -68,6 +68,24 @@ class Opts(dict):
 
     def add(self, string):
 
+        def split(s):
+            """Split a string by , except if in braces"""
+            parts = []
+            bracket_level = 0
+            current = []
+            for c in (s + ','):
+                if c == ',' and bracket_level == 0:
+                    parts.append(''.join(current))
+                    current = []
+                else:
+                    if c == '{':
+                        bracket_level += 1
+                    elif c == '}':
+                        bracket_level -= 1
+                    current.append(c)
+            if bracket_level != 0:
+                raise ValueError('Mismatched braces for ' + s)
+            return parts
 
         if string == '':
             return
@@ -75,7 +93,7 @@ class Opts(dict):
             self['append'] += string[1:] + '\n'
             return
 
-        for part in string.split(','):
+        for part in split(string):
             part = part.strip()
             if part == '':
                 continue
