799695e96b9a244ce3b27a64b84b8f7f0e4873d9
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-02-29 22:05:45 +1300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='lcapy',
-      version='0.13.1',
+      version='0.13.2',
       description='Symbolic linear circuit analysis',
       author='Michael Hayes',
       requires=['sympy', 'numpy', 'scipy'],
