c4d536009dd06ea7ded123dd7dfcd026ec37829c
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-03-07 23:07:26 +1300

    Use new inverter/buffer notation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='lcapy',
-      version='0.13.5',
+      version='0.14.0',
       description='Symbolic linear circuit analysis',
       author='Michael Hayes',
       requires=['sympy', 'numpy', 'scipy'],
