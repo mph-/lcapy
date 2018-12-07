ae7366da2750a2db4324726a1ad81693d6020b06
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-12-07 16:48:12 +1300

    Fix time response

--- demo/oneport/demo1.py
+++ demo/oneport/demo1.py
@@ -1,14 +1,14 @@
-from lcapy import Vdc, R, L, C
+from lcapy import Vstep, R, L, C
 import numpy as np
 from matplotlib.pyplot import figure, savefig, show
 
-a = (Vdc(5) + R(10)) | C(1)
+a = (Vstep(5) + R(10)) | C(1)
 
 t = np.linspace(0, 10, 1000)
 
 fig = figure()
 ax = fig.add_subplot(111)
-ax.plot(t, a.V.impulse_response(t), linewidth=2)
+ax.plot(t, a.v.evaluate(t), linewidth=2)
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Voltage (V)')
 ax.grid(True)
