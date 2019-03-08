0c4906b395f4809d08078df4dfd2d998066d453c
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-03-08 17:13:13 +1300

    Add pins

--- lcapy/schemcpts.py
+++ lcapy/schemcpts.py
@@ -1790,15 +1790,18 @@ class Uadc(Chip):
             'vref-' : ('l', -0.375, -0.25),
             'vref+' : ('l', -0.375, 0.25),                              
             'avss' : ('b', -0.1, -0.5),
+            'vss' : ('b', 0.1, -0.5),            
             'dvss' : ('b', 0.3, -0.5),
             'clk' : ('r', 0.5, -0.25),
             'data' : ('r', 0.5, 0),
             'fs' : ('r', 0.5, 0.25),
             'dvdd' : ('t', 0.3, 0.5),
+            'dvd' : ('t', 0.1, 0.5),            
             'avdd' : ('t', -0.1, 0.5)}
     
     pinlabels = {'vref-' : 'VREF-', 'vref+' : 'VREF+',
-                 'dvss': 'DVSS', 'dvdd' : 'DVDD',
+                 'vss': 'VSS', 'vdd' : 'VDD',
+                 'dvss': 'DVSS', 'dvdd' : 'DVDD',                 
                  'avss': 'AVSS', 'avdd' : 'AVDD',                 
                  'clk' : '>', 'data' : 'DATA', 'fs' : 'FS'}
 
@@ -1828,15 +1831,19 @@ class Udac(Chip):
             'vref-' : ('r', 0.375, -0.25),
             'vref+' : ('r', 0.375, 0.25),
             'avss' : ('b', 0.1, -0.5),
+            'vss' : ('b', -0.1, -0.5),
             'dvss' : ('b', -0.3, -0.5),
             'clk' : ('l', -0.5, -0.25),
             'data' : ('l', -0.5, 0),
             'fs' : ('l', -0.5, 0.25),
             'dvdd' : ('t', -0.3, 0.5),
+            'vdd' : ('t', -0.1, 0.5),            
             'avdd' : ('t', 0.1, 0.5)}
     
-    pinlabels = {'vref-' : 'VREF-', 'vref+' : 'VREF+', 'dvss': 'DVSS',
-                 'dvdd' : 'DVDD', 'avss': 'AVSS', 'avdd' : 'AVDD',
+    pinlabels = {'vref-' : 'VREF-', 'vref+' : 'VREF+',
+                 'vss': 'VSS', 'vdd' : 'VDD',
+                 'dvss': 'DVSS', 'dvdd' : 'DVDD',                 
+                 'avss': 'AVSS', 'avdd' : 'AVDD',
                  'clk' : '>', 'data' : 'DATA', 'fs' : 'FS'}
     
     @property
