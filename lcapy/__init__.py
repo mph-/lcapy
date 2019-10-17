9b8c0ff75a0de0e0e160341995e7f3795f31324a
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-10-17 16:16:07 +1300

    Add comment

--- lcapy/__init__.py
+++ lcapy/__init__.py
@@ -68,6 +68,9 @@ def show_version():
           (python_version, sympy_version, numpy_version,
            matplotlib_version, scipy_version, __version__))
 
+# The following is to help sympify deal with j.   
+# A better fix might be to define an Lcapy clss for j and to
+# use the __sympy_ method.
 from sympy.core.sympify import converter
 from sympy import Symbol
 converter['j'] = j
