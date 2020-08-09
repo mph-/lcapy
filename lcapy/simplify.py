ea0685ef25ac55a2317b2aba25b222d263d3dd22
Michael Hayes <moikl.hayes@gmail.com>
2020-08-09 20:09:16 +1200

    Add simplify module

---
+++ lcapy/simplify.py
@@ -0,0 +1,95 @@
+"""This module contains functions for simplifying expressions.
+
+Copyright 2020 Michael Hayes, UCECE
+
+"""
+
+from sympy import Add, Mul, DiracDelta, Heaviside, Integral
+
+
+def simplify_dirac_delta_term(expr):
+    """ Simplify f(t) * DiracDelta(t) to f(0) * DiracDelta(t)."""
+
+    if not expr.has(DiracDelta):
+        return expr
+    
+    def query(expr):
+
+        return expr.is_Mul and expr.has(DiracDelta)
+
+    def value(expr):
+
+        arg = None
+        factors = expr.args
+        dirac = None
+        parts = []
+        for factor in factors:
+            if factor.is_Function and factor.func == DiracDelta:
+                arg = factor.args[0]
+                dirac = factor
+            else:
+                parts.append(factor)        
+
+        # TODO: handle DiracDelta(symbol + constant).
+        if arg is None or not arg.is_Symbol:
+            return expr
+        const = Mul(*parts).subs(arg, 0)
+        return const * dirac
+
+    return expr.replace(query, value)
+
+
+def simplify_dirac_delta(expr, expand=False):
+    """Simplify f(t) * DiracDelta(t) to f(0) * DiracDelta(t)."""
+
+    if not expr.has(DiracDelta):
+        return expr
+
+    if not expand:
+        return simplify_dirac_delta_term(expr)
+    
+    terms = expr.expand().as_ordered_terms()
+
+    return Add(*[simplify_dirac_delta_term(term) for term in terms])
+
+
+def simplify_heaviside(expr):
+
+    if not expr.has(Heaviside):
+        return expr
+
+    # H(t) x H(t) = H(t)
+
+    if not expr.has(Integral):
+        return expr
+
+    def query(expr):
+
+        if not isinstance(expr, Integral):
+            return False
+        return expr.has(Heaviside)
+
+    def value(expr):
+
+        integrand = expr.args[0]
+        var = expr.args[1][0]
+        lower_limit = expr.args[1][1]
+        upper_limit = expr.args[1][2]
+
+        result = 1
+        for factor in integrand.as_ordered_factors():
+            if isinstance(factor, Heaviside):
+                arg = factor.args[0]
+                if arg == var:
+                    lower_limit = 0
+                    factor = 1
+                elif arg == -var:
+                    upper_limit = 0
+                    factor = 1                    
+            result *= factor
+
+        return Integral(result, (var, lower_limit, upper_limit))
+    
+    expr = expr.replace(query, value)
+    
+    return expr
--- lcapy/sym.py
+++ lcapy/sym.py
@@ -18,6 +18,7 @@ from sympy.core.function import AppliedUndef
 import sympy as sym
 import re
 from .state import state
+from .simplify import simplify_dirac_delta, simplify_heaviside
 
 __all__ = ('symsymbol', 'sympify', 'simplify', 'symbol_delete')
 
@@ -209,93 +210,6 @@ def symsymbol(name, **assumptions):
     """
     return sympify(name, **assumptions)
 
-
-def simplify_dirac_delta_term(expr):
-    """ Simplify f(t) * DiracDelta(t) to f(0) * DiracDelta(t)."""
-
-    if not expr.has(sym.DiracDelta):
-        return expr
-    
-    def query(expr):
-
-        return expr.is_Mul and expr.has(sym.DiracDelta)
-
-    def value(expr):
-
-        arg = None
-        factors = expr.args
-        dirac = None
-        parts = []
-        for factor in factors:
-            if factor.is_Function and factor.func == sym.DiracDelta:
-                arg = factor.args[0]
-                dirac = factor
-            else:
-                parts.append(factor)        
-
-        # TODO: handle DiracDelta(symbol + constant).
-        if arg is None or not arg.is_Symbol:
-            return expr
-        const = sym.Mul(*parts).subs(arg, 0)
-        return const * dirac
-
-    return expr.replace(query, value)
-
-
-def simplify_dirac_delta(expr, expand=False):
-    """Simplify f(t) * DiracDelta(t) to f(0) * DiracDelta(t)."""
-
-    if not expr.has(sym.DiracDelta):
-        return expr
-
-    if not expand:
-        return simplify_dirac_delta_term(expr)
-    
-    terms = expr.expand().as_ordered_terms()
-
-    return sym.Add(*[simplify_dirac_delta_term(term) for term in terms])
-
-
-def simplify_heaviside(expr):
-
-    if not expr.has(sym.Heaviside):
-        return expr
-
-    # H(t) x H(t) = H(t)
-
-    if not expr.has(sym.Integral):
-        return expr
-
-    def query(expr):
-
-        if not isinstance(expr, sym.Integral):
-            return False
-        return expr.has(sym.Heaviside)
-
-    def value(expr):
-
-        integrand = expr.args[0]
-        var = expr.args[1][0]
-        lower_limit = expr.args[1][1]
-        upper_limit = expr.args[1][2]
-
-        result = 1
-        for factor in integrand.as_ordered_factors():
-            if isinstance(factor, sym.Heaviside):
-                arg = factor.args[0]
-                if arg == var:
-                    lower_limit = 0
-                    factor = 1
-                elif arg == -var:
-                    upper_limit = 0
-                    factor = 1                    
-            result *= factor
-
-        return sym.Integral(result, (var, lower_limit, upper_limit))
-    
-    expr = expr.replace(query, value)
-    
-    return expr
 
 def symsimplify(expr):
     """Simplify a SymPy expression.  This is a hack to work around
