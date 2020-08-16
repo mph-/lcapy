4d232e7d76cbbf2f0ae3099517cfabe1d3ce1174
Michael Hayes <moikl.hayes@gmail.com>
2020-08-17 10:13:39 +1200

    V0.66.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-__version__ = '0.65.0-dev'
+__version__ = '0.66.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
