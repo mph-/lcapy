693f078033c802735718e92089173659776a316d
Michael Hayes <moikl.hayes@gmail.com>
2020-09-01 13:40:27 +1200

    Add Foster forms

--- lcapy/synthesis.py
+++ lcapy/synthesis.py
@@ -4,8 +4,8 @@ This module implements experimental network synthesis.
 Copyright 2020 Michael Hayes, UCECE
 """
 
-from .oneport import L, C, R, G
-from .sexpr import s, Zs
+from .oneport import L, C, R, G, Par
+from .sexpr import s, Zs, sExpr
 
 
 # Could make this a mixin for Zs and Ys but would need a Z flavour
@@ -20,6 +20,7 @@ class Synthesis(object):
             return None
 
         var = lexpr.var
+        lexpr = lexpr.partfrac()
         d = lexpr.expr.collect(var, evaluate=False)
 
         net = None
@@ -46,6 +47,7 @@ class Synthesis(object):
             raise ValueError('Not series RC')
 
         var = lexpr.var
+        lexpr = lexpr.partfrac()        
         d = lexpr.expr.collect(var, evaluate=False)
 
         net = None
@@ -72,6 +74,7 @@ class Synthesis(object):
             raise ValueError('Not series LC')
 
         var = lexpr.var
+        lexpr = lexpr.partfrac()
         d = lexpr.expr.collect(var, evaluate=False)
 
         net = None
@@ -102,6 +105,7 @@ class Synthesis(object):
             raise ValueError('Not series RLC')
 
         var = lexpr.var
+        lexpr = lexpr.partfrac()        
         d = lexpr.expr.collect(var, evaluate=False)
 
         net = None
@@ -126,15 +130,159 @@ class Synthesis(object):
         if d != {}:
             raise ValueError('Not series RLC')
 
-        return net        
+        return net
 
-    def FosterI(self, lexpr):
+    def parallelRL(self, lexpr):
+        """Y = 1 / (s * L) + 1 / R"""
+        
+        if lexpr == 0:
+            # Perhaps return Wire object
+            return None
+
+        var = lexpr.var
+        yexpr = (1 / lexpr).partfrac()        
+        d = yexpr.expr.collect(var, evaluate=False)
+
+        net = None
+        a = d.pop(1, None)
+        if a is not None:
+            net = R(1 / a)
+
+        a = d.pop(1 / var, None)
+        if a is not None:
+            if net is None:
+                net = L(1 / a)
+            else:
+                net = net | L(1 / a)            
+
+        if d != {}:
+            raise ValueError('Not parallel RL')
+        
+        return net
+
+    def parallelRC(self, lexpr):
+        """Y = s * C + 1 / R"""
+
+        if lexpr == 0:
+            raise ValueError('Not parallel RC')
+
+        var = lexpr.var
+        yexpr = (1 / lexpr).partfrac()        
+        d = yexpr.expr.collect(var, evaluate=False)
+
+        net = None
+        a = d.pop(1, None)
+        if a is not None:
+            net = R(1 / a)
+
+        a = d.pop(var, None)
+        if a is not None:
+            if net is None:
+                net = C(a)
+            else:
+                net = net | C(a)            
+
+        if d != {}:
+            raise ValueError('Not parallel RC')
+
+        return net
+
+    def parallelLC(self, lexpr):
+        """Y = s * C + 1 / (s * L)"""
+
+        if lexpr == 0:
+            raise ValueError('Not parallel LC')
+
+        var = lexpr.var
+        yexpr = (1 / lexpr).partfrac()        
+        d = yexpr.expr.collect(var, evaluate=False)
+
+        net = None
+        a = d.pop(1, 0)
+        if a != 0:
+            raise ValueError('Not parallel LC')            
+
+        a = d.pop(var, None)
+        if a is not None:
+            net = C(a)
+
+        a = d.pop(1 / var, None)
+        if a is not None:
+            if net is None:
+                net = L(1 / a)
+            else:
+                net = net | L(1 / a)                            
+
+        if d != {}:
+            raise ValueError('Not parallel LC')
+
+        return net
+
+    def parallelRLC(self, lexpr):
+        """Y = s * C + 1 / R + 1 / (s * L)"""
+
+        if lexpr == 0:
+            raise ValueError('Not parallel RLC')
+
+        var = lexpr.var
+        yexpr = (1 / lexpr).partfrac()
+        d = yexpr.expr.collect(var, evaluate=False)
+
+        Rnet = None
+        Lnet = None
+        Cnet = None
+        a = d.pop(1, None)
+        if a is not None:
+            Rnet = R(1 / a)
+
+        a = d.pop(var, None)
+        if a is not None:
+            Cnet = C(a)
+
+        a = d.pop(1 / var, None)
+        if a is not None:
+            Lnet = L(1 / a)                            
+
+        if d != {}:
+            raise ValueError('Not parallel RLC')
+
+        return Par(*[net for net in [Rnet, Lnet, Cnet] if net is not None])
+
+    def RLC(self, lexpr):
+        """Handle series RLC or parallel RLC.  This will fail on R | L + C."""
+        
+        try:
+            return self.seriesRLC(lexpr)
+        except:
+            return self.parallelRLC(lexpr)
+
+    def fosterI(self, lexpr):
 
         expr = lexpr.partfrac(combine_conjugates=True)
 
-        # TODO
+        net = None
+        for term in expr.as_ordered_terms():
+            net1 = self.parallelRLC(sExpr(term))
+            if net is None:
+                net = net1
+            else:
+                net = net + net1
+        return net
+
+    def fosterII(self, lexpr):
+
+        expr = (1 / lexpr).partfrac(combine_conjugates=True)
+
+        net = None
+        for term in expr.as_ordered_terms():
+            net1 = self.seriesRLC(sExpr(1 / term))
+            if net is None:
+                net = net1
+            else:
+                net = net | net1
+        return net    
         
-    def CauerI(self, lexpr):
+    def cauerI(self, lexpr):
 
         # This can result in negative valued components.
         
@@ -157,7 +305,7 @@ class Synthesis(object):
             
             parts = coeff.coeffs()
             if len(parts) > 2:
-                raise ValueError('Cannot express %s as a network' % coeff)
+                raise ValueError('cannot express %s as a network' % coeff)
             
             if n & 1 == 0:
                 if len(parts) == 2:
@@ -189,26 +337,28 @@ class Synthesis(object):
                     net = net1 | net
         return net
 
-    def default(self, lexpr):
-
-        return self.CauerI(lexpr)
-    
     def network(self, lexpr, form='default'):
 
         if form == 'default':
-            form = 'CauerI'
+            form = 'cauerI'
 
         if not isinstance(lexpr, Zs):
             raise ValueError('Expression needs to be Zs object')
 
         # Should test if a positive real function.
         
-        forms = {'CauerI': self.CauerI,
-                 'FosterI': self.FosterI,
+        forms = {'cauerI': self.cauerI,
+                 'fosterI': self.fosterI,
+                 'fosterII': self.fosterII,
+                 'RLC': self.RLC,                 
                  'seriesRL': self.seriesRL,
                  'seriesRC': self.seriesRC,
                  'seriesLC': self.seriesLC,                 
-                 'seriesRLC': self.seriesRLC}
+                 'seriesRLC': self.seriesRLC,
+                 'parallelRL': self.parallelRL,
+                 'parallelRC': self.parallelRC,
+                 'parallelLC': self.parallelLC,                 
+                 'parallelRLC': self.parallelRLC}                 
 
         if form not in forms:
             raise ValueError('Unknown form %s, known forms: %s' % forms.keys())
