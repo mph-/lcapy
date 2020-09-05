84f6c4b4ed6725588de6de9d53eaa2ccab3dc12a
Michael Hayes <moikl.hayes@gmail.com>
2020-09-05 13:49:49 +1200

    Add example ladder networks

---
+++ doc/examples/networks/ladderRC1.py
@@ -0,0 +1,4 @@
+from lcapy import R, C
+n = C('C1') | (R('R1') + (C('C2') | (R('R2') + (C('C3') | R('R3')))))
+n.draw(__file__.replace('.py', '.png'), form='ladder')
+
---
+++ doc/examples/networks/ladderRC2.py
@@ -0,0 +1,4 @@
+from lcapy import R, C
+n = R('R1') + (C('C2') | (R('R2') + (C('C3') | R('R3'))))
+n.draw(__file__.replace('.py', '.png'), form='ladder')
+
