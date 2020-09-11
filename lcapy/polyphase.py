a3333c919fa636ed4720520885d8cc318ebb1574
Michael Hayes <moikl.hayes@gmail.com>
2020-09-11 21:23:42 +1200

    Add VoltageCurrent stacked vectors

--- lcapy/polyphase.py
+++ lcapy/polyphase.py
@@ -39,6 +39,7 @@ class PolyphaseSequence(object):
 
     
 class PhaseVoltageVector(PolyphaseVoltageVector, PolyphasePhase):
+    """These are the phase voltages with respect to a system ground."""
 
     def sequence(self):
         """Convert to sequence voltage vector."""
@@ -51,6 +52,7 @@ class PhaseVoltageVector(PolyphaseVoltageVector, PolyphasePhase):
 
     
 class PhaseCurrentVector(PolyphaseCurrentVector, PolyphasePhase):
+    """These are the phase currents."""    
 
     def sequence(self):
         """Convert to sequence current vector."""        
@@ -106,11 +108,13 @@ def phase_to_line_matrix(N=3):
 
 
 def polyphase_decompose_matrix(N=3, alpha=None):
-    """Matrix to decompose vector of phase components into the
+    """Matrix to decompose vector of phase components into the symmetrical
     sequence components.  The matrix dimension is `N` x `N`.  This
     matrix is equivalent to IDFTmatrix.  The transformation is only
     valid for positive frequency components; the complex conjugate is
-    required for negative frequency components."""
+    required for negative frequency components.
+
+    """
 
     if alpha is None:
         alpha = exp(j * 2 * pi / N)
@@ -121,27 +125,87 @@ def polyphase_decompose_matrix(N=3, alpha=None):
     
     for row in range(N):
         for col in range(N):
-            a[row, col] = alpha ** (row * col)
-    return a / N
+            a[row, col] = alpha ** ((row * col) % N)
+    return a
 
 
-def polyphase_compose_matrix(N=3):
-    """Matrix to compose sequence components into a vector of phase
-    components.  The matrix dimension is `N` x `N`.  This
-    matrix is equivalent to DFTmatrix.  The transformation is only
-    valid for positive frequency components; the complex conjugate is
-    required for negative frequency components."""
+def polyphase_compose_matrix(N=3, alpha=None, invalpha=False):
+    """Matrix to compose symmetrical sequence components into a vector of
+    phase components.  The matrix dimension is `N` x `N`.  This matrix
+    is equivalent to DFTmatrix.  The transformation is only valid for
+    positive frequency components; the complex conjugate is required
+    for negative frequency components.
 
-    alpha = exp(-j * 2 * pi / N)
+    """
+
+    if alpha is None:
+        alpha = exp(j * 2 * pi / N)
+    else:
+        alpha = expr(alpha)    
 
     a = Matrix.zeros(N)
     
     for row in range(N):
         for col in range(N):
-            a[row, col] = alpha ** (row * col)
+            if invalpha:
+                a[row, col] = (1 / alpha) ** ((row * col) % N)
+            else:
+                a[row, col] = alpha ** ((-row * col) % N)
     return a
 
 
 def polyphase_alpha(N):
 
     return exp(-j * 2 * pi / N)
+
+
+
+class PolyphaseVoltageCurrentVector(PolyphaseVector):
+    """This is a stacked vector of voltages and currents."""
+
+    @property
+    def N_phases(self):
+        return self.shape[0] // 2
+    
+    @property
+    def V(self):
+        N = self.N_phases
+        return PolyPhaseVoltageVector(self[0:N])
+
+    @property
+    def I(self):
+        N = self.N_phases
+        return PolyPhaseCurrentVector(self[N:])    
+
+
+class LineVoltageCurrentVector(PolyphaseVoltageCurrentVector):
+    """These are also known as phase to phase voltages."""
+    pass
+
+
+class PhaseVoltageCurrentVector(PolyphaseVoltageCurrentVector):
+    
+    def sequence(self):
+        """Convert to sequence voltageCurrent vector."""
+
+        A = polyphase_decompose_matrix(len(self))
+        return SequenceVoltageCurrentVector(self.vstack(A * self.V, A * self.I))
+
+    def line(self):
+        """Convert to line voltageCurrent vector."""        
+        D = phase_to_line_matrix(len(self))
+
+        return LineVoltageCurrentVector((self.vstack(D * self.V, D * self.I)))
+
+class SequenceVoltageCurrentVector(PolyphaseVoltageCurrentVector):
+
+    def phase(self):
+        """Convert to phase voltageCurrent vector."""
+
+        A = polyphase_compose_matrix(len(self))
+        
+        return PhaseVoltageCurrentVector((self.vstack(A * self.V, A * self.I)))
+
+    def line(self):
+        """Convert to line voltageCurrent vector."""        
+        return self.phase().line()    
