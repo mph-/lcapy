c1c4063347862aceebeb6ec5b6f05fc32c3a8d64
Michael Hayes <moikl.hayes@gmail.com>
2020-09-15 19:37:43 +1200

    Add generic wye-delta transforms

--- lcapy/nettransform.py
+++ lcapy/nettransform.py
@@ -4,6 +4,11 @@ Copyright 2020 Michael Hayes, UCECE
 
 """
 
+from .impedance import Impedance
+from .admittance import Admittance
+from .sexpr import Zs, Ys
+from .oneport import R, Y, Z
+
 
 def Z_wye_to_delta(Z1, Z2, Z3):
     """Perform wye to delta transformation of three impedances.
@@ -58,8 +63,47 @@ def Y_wye_to_delta(Y1, Y2, Y3):
     Yb = Y1 * Y3 / D
     Yc = Y1 * Y2 / D
 
-    return Ya, Yb, Yv
+    return Ya, Yb, Yc
 
 
+def wye_to_delta(cpt1, cpt2, cpt3):
 
+    if isinstance(cpt1, (Impedance, Zs)):
+        return Z_wye_to_delta(cpt1, cpt2, cpt3)
+
+    if isinstance(cpt1, Admittance):
+        return Y_wye_to_delta(cpt1, cpt2, cpt3)    
+
+    if isinstance(cpt1, Z):
+        return [Z(cpt1) for cpt1 in Z_wye_to_delta(cpt1.Z, cpt2.Z, cpt3.Z)]
+
+    if isinstance(cpt1, R):
+        return [R(R1) for R1 in Z_wye_to_delta(cpt1.R, cpt2.R, cpt3.R)]
+
+    if isinstance(cpt1, Y):
+        return [Y(Y1) for Y1 in Y_wye_to_delta(cpt1.Y, cpt2.Y, cpt3.Y)]        
+
+    raise ValueError('Unhandled class %s' % cpt1.__class__.name)
+
+    
+def delta_to_wye(cpt1, cpt2, cpt3):
+
+    if isinstance(cpt1, (Impedance, Zs)):
+        return Z_delta_to_wye(cpt1, cpt2, cpt3)
+
+    if isinstance(cpt1, Admittance):
+        return Y_delta_to_wye(cpt1, cpt2, cpt3)    
+
+    if isinstance(cpt1, Z):
+        return [Z(cpt1) for cpt1 in Z_delta_to_wye(cpt1.Z, cpt2.Z, cpt3.Z)]
+
+    if isinstance(cpt1, R):
+        return [R(R1) for R1 in Z_delta_to_wye(cpt1.R, cpt2.R, cpt3.R)]
+
+    if isinstance(cpt1, Y):
+        return [Y(Y1) for Y1 in Y_delta_to_wye(cpt1.Y, cpt2.Y, cpt3.Y)]        
+
+    raise ValueError('Unhandled class %s' % cpt1.__class__.name)
+
+    
 
