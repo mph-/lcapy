9956c1022cd571ea97a8f7e15fb15fd03307de91
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-12-01 11:26:46 +1300

    Move test requirements into optional feature

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ __version__ = '0.71'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+tests_require = ['nose']
+
 setup(name='lcapy',
       version=__version__,
       author='Michael Hayes',
@@ -23,15 +25,18 @@ setup(name='lcapy',
                         'setuptools',
                         'wheel'
       ],
+      python_requires='>=3.5',
+      extras_require={'test': tests_require},
+      tests_require=tests_require,
       packages=find_packages(exclude=['demo']),
       entry_points={
           'console_scripts': [
               'schtex=lcapy.scripts.schtex:main',
           ],
-      },      
+      },
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
           "Operating System :: OS Independent",
       ],
-)      
+)
