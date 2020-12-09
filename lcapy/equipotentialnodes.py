bb6dd69ea24d7bf94c9fdf0a38437010db697b5b
Michael Hayes <moikl.hayes@gmail.com>
2020-12-10 08:56:51 +1300

    Split out Netlist classes

---
+++ lcapy/equipotentialnodes.py
@@ -0,0 +1,33 @@
+class EquipotentialNodes(dict):
+
+    def add(self, nodenames):
+        for key in nodenames:
+            self[key] = [key]
+
+    def find_key(self, n):
+
+        for key, nodes in self.items():
+            if n in nodes:
+                return key
+        return None
+
+    def add_wire(self, n1, n2):
+
+        key1 = self.find_key(n1)
+        key2 = self.find_key(n2)
+
+        if key1 is None or key2 is None:
+            return
+
+        if key1 != key2:
+            # Merge equipotential nodes.
+            self[key1].extend(self.pop(key2))
+
+    def add_wires(self, nodes):
+
+        # Iterate over all pairs.
+        for i in range(len(nodes)):
+            for m in range(i + 1, len(nodes)):
+                self.add_wire(nodes[i], nodes[m])
+            
+
---
+++ lcapy/subnetlist.py
@@ -0,0 +1,63 @@
+"""This module provides the SubNetlist class.
+
+Copyright 2020 Michael Hayes, UCECE
+
+"""
+
+from .mna import MNAMixin
+from .netlistmixin import NetlistMixin
+from .state import state
+from .symbols import omega
+
+
+
+class SubNetlist(NetlistMixin, MNAMixin):
+    """This is a representation of a netlist for a particular
+    transformation domain, such as ac, dc, transient, or noise."""
+
+    def __new__(cls, netlist, kind):
+
+        obj = netlist.select(kind=kind)
+        # Need own context to avoid conflicts with Vn1 and Vn1(s), etc.
+        obj.context = state.new_context()
+        obj.kind = kind
+        obj.__class__ = cls
+        obj._analysis = obj.analyse()
+        return obj
+
+    def __init__(self, netlist, kind):
+        """ kind can be 't', 'dc', 'ac', 's', 'time', 'ivp', 'n*', omega, 
+        or an integer"""
+        
+        if kind == omega:
+            return
+        if not isinstance(kind, str):
+            return
+        if kind[0] == 'n':
+            return
+        kinds = ('t', 'dc', 'ac', 's', 'time', 'ivp', 'laplace')
+        if kind not in kinds:
+            raise ValueError('Expected one of %s for kind, got %s' %
+                             (', '.join(kinds), kind))
+
+    def get_I(self, name):
+        """Current through component"""
+
+        self._solve()
+        return self._Idict[name].canonical()
+
+    def get_i(self, name):
+        """Time-domain current through component"""
+
+        return self.get_I(name).time()
+
+    def get_Vd(self, Np, Nm=None):
+        """Voltage drop between nodes"""
+
+        self._solve()
+        return (self._Vdict[Np] - self._Vdict[Nm]).canonical()
+
+    def get_vd(self, Np, Nm=None):
+        """Time-domain voltage drop between nodes"""
+
+        return self.get_Vd(Np, Nm).time()
