04e16ae4d3a5a48d16b6da28b095a9c28d237e58
Michael Hayes <moikl.hayes@gmail.com>
2021-01-14 19:50:56 +1300

    Create Vac and Vdc

--- lcapy/voltagemixin.py
+++ lcapy/voltagemixin.py
@@ -8,8 +8,15 @@ class VoltageMixin(Quantity):
     is_voltage = True
 
     def cpt(self):
-        from .oneport import V
-        return V(self)
+        from .oneport import V, Vac, Vdc
+
+        if self.is_dc:
+            return Vdc(self.expr)
+        elif self.is_ac:
+            p = self.as_phasor()
+            return Vac(p.expr, omega=p.omega)
+        
+        return V(self.expr)
 
     
     
