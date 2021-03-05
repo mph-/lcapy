86147154895e9bb840997dfec93198ae713dc08d
Michael Hayes <moikl.hayes@gmail.com>
2021-03-06 11:28:02 +1300

    Remove cruft

--- lcapy/schemgraphplacer.py
+++ lcapy/schemgraphplacer.py
@@ -7,7 +7,6 @@ Copyright 2021 Michael Hayes, UCECE
 from .schemgraph import Graph
 from .schemmisc import Pos
 from .schemplacerbase import SchemPlacerBase
-from numpy import argsort
 
 # Components are positioned using two graphs; one graph for
 # the x direction and the other for the y direction. 
@@ -39,87 +38,3 @@ class SchemGraphPlacer(SchemPlacerBase):
         self.xgraph = Graph('horizontal', nodes, debug)
         self.ygraph = Graph('vertical', nodes, debug)
 
-    def _xlink(self, elt, graph):
-
-        xvals = elt.xvals
-        nodes = elt.nodes
-        for m1, n1 in enumerate(nodes):
-            for m2, n2 in enumerate(nodes[m1 + 1:], m1 + 1):
-                if xvals[m2] == xvals[m1]:
-                    graph.link(n1.name, n2.name)
-
-    def _ylink(self, elt, graph):
-
-        yvals = elt.yvals
-        nodes = elt.nodes        
-        for m1, n1 in enumerate(nodes):
-            for m2, n2 in enumerate(nodes[m1 + 1:], m1 + 1):
-                if yvals[m2] == yvals[m1]:
-                    graph.link(n1.name, n2.name)
-
-    def _place(self, elt, graph, vals):
-
-        if elt.free:
-            return        
-
-        if elt.offset != 0:
-            print('TODO: offset %s by %f' % (elt, elt.offset))
-        
-        size = elt.size
-        nodes = elt.nodes                
-        idx = argsort(vals)[::-1]
-        for i in range(len(idx) - 1):
-            m1 = idx[i]
-            m2 = idx[i + 1]
-            n1 = nodes[m1]
-            n2 = nodes[m2]
-            value = (vals[m2] - vals[m1]) * size
-            graph.add(elt, n1.name, n2.name, value, elt.stretch)
-
-    def _xplace(self, elt, graph):
-
-        if elt.place:
-            self._place(elt, graph, elt.xvals)
-
-    def _yplace(self, elt, graph):
-        
-        if elt.place:        
-            self._place(elt, graph, elt.yvals)
-
-    def _make_graphs(self):
-
-        # The x and y positions of a component node are determined
-        # independently.  The principle is that each component has a
-        # minimum size (usually 1 but changeable with the size option)
-        # but its wires can be stretched.
-
-        # When solving the x position, first nodes that must be
-        # vertically aligned (with the up or down option) are combined
-        # into a set.  Then the left and right options are used to
-        # form a graph.  This graph is traversed to find the longest
-        # path and in the process each node gets assigned the longest
-        # distance from the root of the graph.  To centre components,
-        # a reverse graph is created and the distances are averaged.
-
-
-        # Use components in orthogonal directions as constraints.  The
-        # nodes of orthogonal components get combined into a
-        # common node.
-        for m, elt in enumerate(self.elements.values()):
-
-            if elt.offset != 0:
-                raise ValueError('offset field should be removed')
-            if elt.directive or elt.ignore or not elt.place or elt.free:
-                continue
-            
-            self._xlink(elt, self.xgraph)
-            self._ylink(elt, self.ygraph)
-
-        # Now form forward and reverse directed graph using components
-        # in the desired directions.
-        # Note, this must be done after the linking step.
-        for m, elt in enumerate(self.elements.values()):
-            if elt.directive or elt.ignore:
-                continue            
-            self._xplace(elt, self.xgraph)
-            self._yplace(elt, self.ygraph)
--- lcapy/schemplacerbase.py
+++ lcapy/schemplacerbase.py
@@ -1,9 +1,89 @@
 from .schemmisc import Pos
-
+from numpy import argsort
 
 class SchemPlacerBase(object):
 
+    def _xlink(self, elt):
 
+        xvals = elt.xvals
+        nodes = elt.nodes
+        for m1, n1 in enumerate(nodes):
+            for m2, n2 in enumerate(nodes[m1 + 1:], m1 + 1):
+                if xvals[m2] == xvals[m1]:
+                    self.xgraph.link(n1.name, n2.name)
+
+    def _ylink(self, elt):
+
+        yvals = elt.yvals
+        nodes = elt.nodes        
+        for m1, n1 in enumerate(nodes):
+            for m2, n2 in enumerate(nodes[m1 + 1:], m1 + 1):
+                if yvals[m2] == yvals[m1]:
+                    self.ygraph.link(n1.name, n2.name)
+
+    def _place(self, elt, graph, vals):
+
+        if elt.free:
+            return        
+
+        if elt.offset != 0:
+            print('TODO: offset %s by %f' % (elt, elt.offset))
+        
+        size = elt.size
+        nodes = elt.nodes                
+        idx = argsort(vals)[::-1]
+        for i in range(len(idx) - 1):
+            m1 = idx[i]
+            m2 = idx[i + 1]
+            n1 = nodes[m1]
+            n2 = nodes[m2]
+            value = (vals[m2] - vals[m1]) * size
+            graph.add(elt, n1.name, n2.name, value, elt.stretch)
+
+    def _xplace(self, elt):
+
+        self._place(elt, self.xgraph, elt.xvals)
+
+    def _yplace(self, elt):
+        
+        self._place(elt, self.ygraph, elt.yvals)
+
+    def _make_graphs(self):
+
+        # The x and y positions of a component node are determined
+        # independently.  The principle is that each component has a
+        # minimum size (usually 1 but changeable with the size option)
+        # but its wires can be stretched.
+
+        # When solving the x position, first nodes that must be
+        # vertically aligned (with the up or down option) are combined
+        # into a set.  Then the left and right options are used to
+        # form a graph.  This graph is traversed to find the longest
+        # path and in the process each node gets assigned the longest
+        # distance from the root of the graph.  To centre components,
+        # a reverse graph is created and the distances are averaged.
+
+        # Use components in orthogonal directions as constraints.  The
+        # nodes of orthogonal components get combined into a
+        # common node.
+        for m, elt in enumerate(self.elements.values()):
+
+            if elt.offset != 0:
+                raise ValueError('offset field should be removed')
+            if elt.directive or elt.ignore or not elt.place or elt.free:
+                continue
+            
+            self._xlink(elt)
+            self._ylink(elt)
+
+        # Now form forward and reverse directed graph using components
+        # in the desired directions.
+        # Note, this must be done after the linking step.
+        for m, elt in enumerate(self.elements.values()):
+            if elt.directive or elt.ignore or elt.free:
+                continue            
+            self._xplace(elt)
+            self._yplace(elt)
 
     def solve(self, node_spacing):
 
