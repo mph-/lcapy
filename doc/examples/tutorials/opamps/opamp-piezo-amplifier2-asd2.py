f9c42a3ff49248cada0d3d94e64c1412c16bb260
Michael Hayes <moikl.hayes@gmail.com>
2021-03-29 09:03:10 +1300

    Tweak C values

--- doc/examples/tutorials/opamps/opamp-piezo-amplifier2-asd2.py
+++ doc/examples/tutorials/opamps/opamp-piezo-amplifier2-asd2.py
@@ -5,8 +5,8 @@ a = Circuit('opamp-piezo-amplifier1.sch')
 
 Vo = a.Po.V.n(f).limit('A', oo)
 
-defs1 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-3, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
-defs2 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
+defs1 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
+defs2 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':10e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
 defs3 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':1e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
 
 Vo1 = Vo.subs(defs1)
@@ -15,8 +15,8 @@ Vo3 = Vo.subs(defs3)
 
 vf = logspace(0, 5, 201)
 
-ax = Vo1.plot(vf, log_frequency=True, yscale=1e9, label='C=100 mF' )
-ax = Vo2.plot(vf, log_frequency=True, yscale=1e9, label='C=100 uF', axes=ax)
+ax = Vo1.plot(vf, log_frequency=True, yscale=1e9, label='C=100 uF' )
+ax = Vo2.plot(vf, log_frequency=True, yscale=1e9, label='C=10 uF', axes=ax)
 Vo3.plot(vf, log_frequency=True, yscale=1e9, label='C=1 uF', ylabel='Voltage noise ASD (nV/rtHz)', axes=ax)
 ax.legend()
 
--- doc/examples/tutorials/opamps/opamp-piezo-amplifier2-frequency-response1.py
+++ doc/examples/tutorials/opamps/opamp-piezo-amplifier2-frequency-response1.py
@@ -5,8 +5,8 @@ a = Circuit('opamp-piezo-amplifier1.sch')
 
 H = a.transfer('Cs', 'Po')(f).limit('A', oo)
 
-defs1 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-3, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
-defs2 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
+defs1 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':100e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
+defs2 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':10e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
 defs3 = {'R1':100, 'R2':900, 'Cs':1e-9, 'Rs':100e6, 'C':1e-6, 'Vn':2e-9, 'Inp':5e-15, 'Inn':5e-15}
 
 H1 = H.subs(defs1)
@@ -15,8 +15,8 @@ H3 = H.subs(defs3)
 
 vf = logspace(0, 5, 201)
 
-ax = H1.plot(vf, log_frequency=True, label='C=100 mF' )
-ax = H2.plot(vf, log_frequency=True, label='C=100 uF', axes=ax)
+ax = H1.plot(vf, log_frequency=True, label='C=100 uF' )
+ax = H2.plot(vf, log_frequency=True, label='C=10 uF', axes=ax)
 H3.plot(vf, log_frequency=True, label='C=1 uF', ylabel='Gain (dB)', axes=ax)
 ax.legend()
 
