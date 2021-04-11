86d78f8058bfdfc2ad3ec86e367c4705726ecbec
Michael Hayes <moikl.hayes@gmail.com>
2021-04-11 21:02:25 +1200

    V0.85

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '0.85dev'
+__version__ = '0.85'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
