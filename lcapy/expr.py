c72086553e5b6130bdd1c38ce2e19009e83a4328
Michael Hayes <moikl.hayes@gmail.com>
2021-04-18 13:57:51 +1200

    Fix is_complex

--- lcapy/expr.py
+++ lcapy/expr.py
@@ -593,9 +593,9 @@ class Expr(UndefinedDomain, UndefinedQuantity, ExprPrint, ExprMisc):
     @property
     def is_complex(self):
         
-        if 'complex' not in self.assumptions:
-            return False
-        return self.assumptions['complex'] == True
+        if 'complex' in self.assumptions:
+            return self.assumptions['complex'] == True            
+        return self.has(j) or self.has(s)
 
     @property
     def is_conditional(self):
