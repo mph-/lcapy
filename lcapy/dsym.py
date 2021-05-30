b6fa1c145351dc5f74696ab057d01be3f647d553
Michael Hayes <moikl.hayes@gmail.com>
2021-05-31 09:30:27 +1200

    Ensure that n and k are integer

--- lcapy/dsym.py
+++ lcapy/dsym.py
@@ -1,13 +1,13 @@
 """This module defines discrete-time symbols.
 
-Copyright 2020 Michael Hayes, UCECE
+Copyright 2020---2021 Michael Hayes, UCECE
 
 """
 
 from .sym import symsymbol
 
-nsym = symsymbol('n', real=True)
-ksym = symsymbol('k', real=True)
+nsym = symsymbol('n', integer=True)
+ksym = symsymbol('k', integer=True)
 zsym = symsymbol('z', real=False)
 
 dt = symsymbol('Delta_t', real=True)
