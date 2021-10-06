ad1924c9e1f2acd38246efd74c75cbd6e939a2a2
Michael Hayes <moikl.hayes@gmail.com>
2021-10-06 13:40:15 +1300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '0.99'
+__version__ = '1.00dev'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
