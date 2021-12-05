cbabf945df3b1dfcb0ad380d28723f53ceae6332
Michael Hayes <moikl.hayes@gmail.com>
2021-12-05 22:01:53 +1300

    Add test_simulation

---
+++ lcapy/tests/test_simulation.py
@@ -0,0 +1,26 @@
+from lcapy import *
+import numpy as np
+import unittest
+
+
+class LcapyTester(unittest.TestCase):
+
+    """Unit tests for lcapy
+
+    """
+
+    def test_sim(self):
+
+        cct = Circuit("""
+        V1 1 0 step 10; down
+        R1 1 2 5; right
+        L1 2 0_2 2; down
+        W 0 0_2; right""")
+
+        results = cct.sim(np.linspace(0, 10))
+
+        self.assertEqual(results.R1.v[0], 0, 'v[0]')
+        self.assertEqual(np.round(results.R1.v[-1], 3), 10, 'v[-1]')
+        
+
+        
