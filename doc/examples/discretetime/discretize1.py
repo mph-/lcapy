a309d25af9b1196c8c399e66461e444c89fd2d54
Michael Hayes <moikl.hayes@gmail.com>
2021-12-31 22:16:09 +1300

    Tweak plot

--- doc/examples/discretetime/discretize1.py
+++ doc/examples/discretetime/discretize1.py
@@ -12,11 +12,11 @@ h5 = h(n, method='backward-euler').subs(dt, 0.1)
 
 fig, axes = subplots(1, figsize=(6, 3.5))
 
-h1.plot((-10, 20), axes=axes, label='impulse-invariance', color='C0')
-h2.plot((-10, 20), axes=axes, label='matched-Z', color='C1')
-h3.plot((-10, 20), axes=axes, label='bilinear', color='C2')
-h4.plot((-10, 20), axes=axes, label='forward-Euler', color='C3')
-h5.plot((-10, 20), axes=axes, label='backward-Euler', color='C4')
+h1.plot((-5, 15), axes=axes, label='impulse-invariance', color='C0')
+h2.plot((-5, 15), axes=axes, label='matched-Z', color='C1')
+h3.plot((-5, 15), axes=axes, label='bilinear', color='C2')
+h4.plot((-5, 15), axes=axes, label='forward-Euler', color='C3')
+h5.plot((-5, 15), axes=axes, label='backward-Euler', color='C4')
 axes.set_ylim(0, 1.5)
 
 axes.legend()
