5f05bade85b17e1814deb35f983d20e3665243e9
Michael Hayes <moikl.hayes@gmail.com>
2022-01-27 13:49:11 +1300

    Add __eq__

--- lcapy/diffeq.py
+++ lcapy/diffeq.py
@@ -13,8 +13,8 @@ class DifferenceEquation(DiscreteTimeDomainExpression):
     def __init__(self, lhs, rhs, inputsym='x', outputsym='y', **assumptions):
 
         lhs = nexpr(lhs, **assumptions)
-        rhs = nexpr(rhs, **assumptions)        
-        
+        rhs = nexpr(rhs, **assumptions)
+
         lhssymbols = lhs.symbols
         rhssymbols = rhs.symbols
 
@@ -27,18 +27,25 @@ class DifferenceEquation(DiscreteTimeDomainExpression):
         eqn = sym.Eq(lhs.expr, rhs.expr, evaluate=False)
         super (DifferenceEquation, self).__init__(cls(eqn), **assumptions)
         self.inputsym = inputsym
-        self.outputsym = outputsym        
+        self.outputsym = outputsym
+
+    def __eq__(self, x):
+
+        if not isinstance(x, self.__class__):
+            return False
+
+        return self.lhs == x.lhs and self.rhs == x.rhs
 
     def transfer_function(self):
         """Create discrete-time transfer function."""
         from .zexpr import zexpr
-        
-        X = self.inputsym.upper()        
+
+        X = self.inputsym.upper()
         Y = self.outputsym.upper()
 
         result = self.ZT().solve(Y)[0] / zexpr(X + '(z)')
         return result
-    
+
     @property
     def lhs(self):
         return DiscreteTimeDomainExpression(self.expr.lhs, **self.assumptions)
@@ -46,12 +53,12 @@ class DifferenceEquation(DiscreteTimeDomainExpression):
     @property
     def rhs(self):
         return DiscreteTimeDomainExpression(self.expr.rhs, **self.assumptions)
-    
+
     def dlti_filter(self):
         """Create discrete-time linear time-invariant filter."""
 
         return self.transfer_function().dlti_filter()
-    
+
     def separate(self):
         """Rewrite difference equation so that input symbols are on the right
         and output symbols are on the left."""
@@ -67,12 +74,10 @@ class DifferenceEquation(DiscreteTimeDomainExpression):
                 newrhs -= term
 
         for term in self.rhs.as_ordered_terms():
-            symbols = nexpr(term).symbols            
+            symbols = nexpr(term).symbols
             if self.outputsym in symbols:
                 newlhs -= term
             if self.inputsym in symbols:
                 newrhs += term
 
         return self.__class__(newlhs, newrhs, **self.assumptions)
-    
-                
