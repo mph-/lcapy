f14954a564572046deb7a17742d6fd70e0c54c21
Michael Hayes <moikl.hayes@gmail.com>
2022-02-01 22:40:35 +1300

    Try cached_property

--- lcapy/cache.py
+++ lcapy/cache.py
@@ -7,6 +7,9 @@ try:
     # Requires python3.8
     from functools import cached_property
 except:
-    from property_cached import cached_property
-    
+    try:
+        from property_cached import cached_property
+    except:
+        from cached_property import cached_property
+
 from functools import lru_cache
