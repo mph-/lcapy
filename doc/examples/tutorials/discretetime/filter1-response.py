54f54767422f7bd6eff164b86e7e0d19c851b591
Michael Hayes <moikl.hayes@gmail.com>
2022-03-03 08:14:56 +1300

    Fix plot

--- doc/examples/tutorials/discretetime/filter1-response.py
+++ doc/examples/tutorials/discretetime/filter1-response.py
@@ -4,6 +4,6 @@ from lcapy import Circuit, f
 a = Circuit('filter1.sch')
 H = a.transfer('P1', 'P2')
 Hv = H.subs({'R1':22, 'C1':100e-9, 'R2':1e6, 'C2':1e-9})
-Hv.bode_plot((0.1, 10e3), dbmax=5)
+Hv.bode_plot((0.1, 10e3))
 
 savefig(__file__.replace('.py', '.png'), bbox_inches='tight')
