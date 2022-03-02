d06ca9eea7e5d98278066187428a0ec19d295ddf
Michael Hayes <moikl.hayes@gmail.com>
2022-03-02 23:42:45 +1300

    Add filter response

---
+++ doc/examples/tutorials/discretetime/filter1-response.py
@@ -0,0 +1,9 @@
+from matplotlib.pyplot import savefig
+from lcapy import Circuit, f
+
+a = Circuit('filter1.sch')
+H = a.transfer('P1', 'P2')
+Hv = H.subs({'R1':22, 'C1':100e-9, 'R2':1e6, 'C2':1e-9})
+Hv.bode_plot((0.1, 10e3), dbmax=5)
+
+savefig(__file__.replace('.py', '.png'), bbox_inches='tight')
