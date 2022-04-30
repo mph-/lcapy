07fad0ab9b29cfe21345f323ea58f534c1da288a
Michael Hayes <moikl.hayes@gmail.com>
2022-05-01 10:58:28 +1200

    Fix name and value formatting

--- lcapy/schematic.py
+++ lcapy/schematic.py
@@ -297,6 +297,23 @@ class Schematic(NetfileMixin):
 
         return node
 
+    def _format_name(self, cpt_type, cpt_id):
+
+        name = cpt_type
+        if len(name) > 1:
+            name = r'\mathrm{%s}' % name
+        if cpt_id != '':
+            name = name + '_{%s}' % cpt_id
+        return latex_format_label('$' + name + '$')
+
+    def _format_expr(self, expr):
+
+        return Expr(expr, cache=False).latex_math()
+
+    def _format_value_units(self, value, units):
+
+        return EngFormatter().latex_math(value, units)
+
     def _cpt_add(self, cpt):
 
         if cpt.offset != 0 and len(cpt.node_names) == 2:
@@ -337,26 +354,11 @@ class Schematic(NetfileMixin):
             self.add('%s; %s' % (net, cpt.opts))
             return
 
-        def tex_name(name, subscript=None):
-
-            if subscript is None:
-                subscript = ''
-
-            if len(name) > 1:
-                name = r'\mathrm{%s}' % name
-            if len(subscript) > 1:
-                subscript = r'\mathrm{%s}' % subscript
-
-            if len(subscript) == 0:
-                return '$' + name + '$'
-
-            return '$%s_{%s}$' % (name, subscript)
-
         # There are two possible labels for a component:
-        # 1. Component identifier, e.g., R1
+        # 1. Component name, e.g., R1
         # 2. Component value, expression, or symbol
 
-        id_label = tex_name(cpt.type, cpt.id)
+        id_label = self._format_name(cpt.type, cpt.id)
         value_label = None
 
         if cpt.type in ('O', 'P', 'W') or id_label.find('#') != -1:
@@ -365,6 +367,7 @@ class Schematic(NetfileMixin):
         if cpt.type in ('S', 'SW', 'U'):
             value_label = ''
 
+        unify = False
         if cpt.args != ():
 
             # TODO, extend for mechanical and acoustical components.
@@ -374,9 +377,9 @@ class Schematic(NetfileMixin):
             expr = cpt.args[0]
             if cpt.classname in ('Vstep', 'Istep'):
                 expr = '(%s) * Heaviside(t)' % expr
-                value_label = Expr(expr, cache=False).latex_math()
+                value_label = self._format_expr(expr)
             elif cpt.classname in ('Vs', 'Is'):
-                value_label = Expr(expr, cache=False).latex_math()
+                value_label = self._format_expr(expr)
             elif cpt.classname == 'TF':
                 expr = sym.sympify(expr)
                 if expr.is_Pow and expr.args[1] == -1:
@@ -388,29 +391,36 @@ class Schematic(NetfileMixin):
                 # control current is specified by a voltage source.
                 # The user will have to override manually.
                 expr = cpt.args[1]
-                value_label = Expr(expr, cache=False).latex_math()
+                value_label = self._format_expr(expr)
             elif cpt.classname not in ('TP',):
                 try:
                     # Handle things like 9/1000 that can occur
                     # when substituting cpt values.
                     value = float(sym.Rational(expr))
                     if cpt.type in units_map:
-                        value_label = EngFormatter().latex_math(
+                        value_label = self._format_value_units(
                             value, units_map[cpt.type])
                     else:
-                        value_label = Expr(expr, cache=False).latex_math()
+                        value_label = self._format_expr(expr)
 
                 except (ValueError, TypeError):
                     # This catches non numeric arg.
-                    value_label = Expr(expr, cache=False).latex_math()
+                    value_label = self._format_expr(expr)
 
-        # Currently, we only annnotate the component with the value,
-        # expression, or symbol.  If this is not specified, it
-        # defaults to the component identifier.  Note, some objects
-        # we do not want to label, such as wires and ports.
+            # Enssure labels are the same when the value is not specified.
+            # This will prevent printing the name and value.
+            unify = expr == cpt.type + cpt.id
+
+            # Currently, we only annnotate the component with the value,
+            # expression, or symbol.  If this is not specified, it
+            # defaults to the component identifier.  Note, some objects
+            # we do not want to label, such as wires and ports.
         cpt.id_label = '' if id_label is None else latex_format_label(id_label)
         cpt.value_label = cpt.id_label if value_label is None else latex_format_label(
             value_label)
+
+        if unify:
+            cpt.id_label = cpt.value_label
 
         if cpt.opts_string != '':
             self.hints = True
