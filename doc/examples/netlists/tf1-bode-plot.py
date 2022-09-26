43fc04821731dbc478d113249d16dea90f220ef4
Michael Hayes <moikl.hayes@gmail.com>
2022-09-26 14:40:47 +1300

    Evaluate at j2pif

--- doc/examples/netlists/tf1-bode-plot.py
+++ doc/examples/netlists/tf1-bode-plot.py
@@ -1,10 +1,12 @@
-from lcapy import s, j, pi, f, transfer
+from lcapy import s, j, pi, f, transfer, j2pif
 from matplotlib.pyplot import savefig
 from numpy import logspace
 
+# Note, this has a marginally stable impulse response since it has a
+# pole at s = 0.
 H = transfer((s - 2) * (s + 3) / (s * (s - 2 * j) * (s + 2 * j)))
 
 fv = logspace(-1, 3, 400)
-H(f).dB.plot(fv, log_scale=True)
+H(j2pif).dB.plot(fv, log_scale=True)
 
 savefig('tf1-bode-plot.png')
--- doc/examples/networks/series-RLC3-Z.py
+++ doc/examples/networks/series-RLC3-Z.py
@@ -5,6 +5,6 @@ from matplotlib.pyplot import savefig
 N = R(10) + C(1e-4) + L(1e-3)
 
 vf = logspace(0, 5, 400)
-N.Z(f).magnitude.plot(vf)
+N.Z(j2pif).magnitude.plot(vf)
 
 savefig('series-RLC3-Z.png')
