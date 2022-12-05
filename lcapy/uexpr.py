6832a95dfd7880e044eac603019d9b50f9a2902a
Michael Hayes <moikl.hayes@gmail.com>
2022-12-05 13:54:34 +1300

    Add plot method for UndefinedDoaminExpression

--- lcapy/uexpr.py
+++ lcapy/uexpr.py
@@ -24,9 +24,29 @@ class UndefinedDomainExpression(UndefinedDomain, Expr):
         if not self.has(self.var):
             warn('Expression %s does not contain variable %s' % (self, self.var))
 
+    def plot(self, x=None, xlabel='', **kwargs):
+        """Plot the expression.  If x is not specified, it defaults to the
+        range(-0.2, 2).  x can be a vector of specified instants, a
+        tuple specifing the range, or a constant specifying the
+        maximum value with the minimum value set to 0.
+
+        kwargs include:
+        `axes` - the plot axes to use otherwise a new figure is created
+        `xlabel` - the x-axis label
+        `ylabel` - the y-axis label
+        `xscale` - the x-axis scaling, say for plotting as ms
+        `yscale` - the y-axis scaling, say for plotting mV
+        in addition to those supported by the matplotlib plot command.
+
+        The plot axes are returned."""
+
+        from .plot import plot_time
+        return plot_time(self, x, **kwargs)
+
 
 def uexpr(arg, var, **assumptions):
-    """Create UndefinedDomainExpression object."""
+    """Create UndefinedDomainExpression object where `var` is the
+    independent variable."""
 
     assumptions['var'] = var
     return expr_make('undefined', arg, **assumptions)
