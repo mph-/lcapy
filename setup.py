4630a67b457c247204a59a947fa16fcfca0a4816
Michael Hayes <moikl.hayes@gmail.com>
2024-02-09 19:55:13 +1300

    Update deps

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(name='lcapy',
       python_requires='>=3.7',  # >=3.6 should still work but not tested
       extras_require={
           'test': tests_require,
-          'doc': ['sphinx', 'ipython', 'sphinx-rtd-theme'],
+          'doc': ['sphinx>=7', 'ipython', 'sphinx-rtd-theme', 'docutils',
+                  'pygments'],
           'release': ['wheel', 'twine'],
       },
       tests_require=tests_require,
