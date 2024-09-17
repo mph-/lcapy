b02f82b525d5949e337b921cc336b2ddb76b62a0
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-09-17 11:20:29 +0200

    add tpyes to simplificationStep

--- lcapy/solutionStep.py
+++ lcapy/solutionStep.py
@@ -1,19 +1,20 @@
 from lcapy import Circuit
 from lcapy import DrawWithSchemdraw
 from lcapy.componentRelation import ComponentRelation
+from typing import Union
 
 
 class SolutionStep:
     def __init__(self, step: tuple):
-        self.circuit = step[0]
-        self.cpt1 = step[1]
-        self.cpt2 = step[2]
-        self.newCptName = step[3]
+        self.circuit: Circuit = step[0]
+        self.cpt1: Union[str, None] = step[1]
+        self.cpt2: Union[str, None] = step[2]
+        self.newCptName: Union[str, None] = step[3]
         self.relation: ComponentRelation = step[4]
-        self.isInitialStep = not (step[1] or step[2] or step[3] or step[4])
-        self.solutionText = None
-        self.lastStep = None
-        self.nextStep = None
+        self.isInitialStep: bool = not (step[1] or step[2] or step[3] or step[4])
+        self.solutionText: Union[str, None] = None
+        self.lastStep: Union[Circuit, None] = None
+        self.nextStep: Union[Circuit, None] = None
 
     def draw(self):
         DrawWithSchemdraw(self.circuit).draw()
