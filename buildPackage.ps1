b51c13d47e824e0aadc6450996160e11b1067bd2
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-09-17 15:59:45 +0200

    adjust buildSkript to use new python interpreter

--- buildPackage.ps1
+++ buildPackage.ps1
@@ -4,8 +4,9 @@ $curDir = Get-Location
 Write-Host "Execute test circuits" -ForegroundColor Green
 Start-Sleep -Milliseconds 500
 Set-Location $PSScriptRoot/Non-Lcapy-Files
+Set-Alias pythonPath C:\Users\yannick.wieland\AppData\Local\Programs\Python\lcapy1_24\Scripts\python.exe
 
-python TestWithStandardCircuits.py
+pythonPath TestWithStandardCircuits.py
 if ($LASTEXITCODE -ne 0){
     Write-Host "An error occured while simplifiing the test circuits" -ForegroundColor Red
     Set-Location $curDir
@@ -17,7 +18,7 @@ Set-Location $PSScriptRoot
 Write-Host "Building package" -ForegroundColor Green
 Start-Sleep -Milliseconds 1000
 
-python setup.py sdist bdist_wheel
+pythonPath setup.py sdist bdist_wheel
 if ($LASTEXITCODE -ne 0){
     Write-Host "An error occured while building the package" -ForegroundColor Red
     Set-Location $curDir
