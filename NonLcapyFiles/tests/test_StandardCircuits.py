a679e4adeeda7e3f21bec099f5abe7294b70fdc7
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-09-24 08:16:57 +0200

    rearange files to get structure for pytest, rename Current Tests to pytest pattern, add init-file to include solve in pyFiles

---
+++ NonLcapyFiles/tests/test_StandardCircuits.py
@@ -0,0 +1,53 @@
+from NonLcapyFiles.solve import solve_circuit, SolveInUserOrder
+import os
+import random
+
+# string is filename, integer is number of steps that shall be created
+# the initial step has to be included
+filenames = [("Circuit_inductors.txt", 5),
+             ("Circuit_resistors.txt", 5),
+             ("Circuit_capacitors.txt", 5),
+             ("Circuit_mixed_2pi30.txt", 5),
+             ("Circuit_mixed_omega0.txt", 5),
+             ("Circuit_mixed_30.txt", 5),
+             ("Circuit_mixed.txt", 5)]
+
+
+def clearDir(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        toRemove = os.listdir(path)
+        for remove in toRemove:
+            os.remove(os.path.join(path, remove))
+
+
+def check_for_solutions(solSteps: int, filename: str, path: str = "../Solutions", filesPerStep: int = 3):
+    # each step produces 2 json files and 1 svg file
+    assert len(os.listdir(path)) == solSteps*filesPerStep, f"{filename} didn't produce as many files as expected"
+
+
+def solveInUserOrder(filename):
+    clearDir("../Solutions")
+
+    test = SolveInUserOrder(filename=filename, savePath="../Solutions/", filePath="../StandardCircuits/")
+    test.createInitialStep()
+
+    test.simplifyTwoCpts(["Z4", "Z5"])
+    test.simplifyTwoCpts(["Z1", "Zsim1"])
+    test.simplifyTwoCpts(["Z2", "Z3"])
+    test.simplifyTwoCpts(["Zsim2", "Zsim3"])
+
+
+def test_solve_circuits():
+    for filename in filenames:
+        clearDir("../Solutions")
+        solve_circuit(filename[0], filePath="../StandardCircuits/", savePath="../Solutions")
+        check_for_solutions(filename=filename[0], solSteps=filename[1])
+        print(f"{filename[0]+'...':40s}success")
+
+
+def test_SolveInUserOrder():
+    for filename in filenames:
+        clearDir("../Solutions")
+        solveInUserOrder(filename[0])
+        check_for_solutions(filename=filename[0], solSteps=filename[1])
+        print(f"{filename[0]+'...':40s}success")
--- buildPackage.ps1
+++ buildPackage.ps1
@@ -3,9 +3,13 @@ param (
 )
 if($pythonPath){
     Set-Alias pythonPath $pythonPath
+    $pytestPath = Split-Path -Path pythonPath
+    $pytestPath = Join-Path -Path $pytestPath -ChildPath "pytest.exe"
+    Set-Alias pytest $pytestPath
 }
 else{
     Set-Alias pythonPath python
+    Set-Alias pytest pytest
 }
 $output = [string]::Concat("executing with: ", (Get-Alias pythonPath).Definition, "`npython refers to the standard python installation or the current aktiv venv")
 Write-Output $output
@@ -13,13 +17,13 @@ Write-Output $output
 $startDir = Get-Location
 
 # Make sure the test circuits can be executed before building the Package
-Write-Host "Execute test circuits" -ForegroundColor Green
+Write-Host "Execute tests" -ForegroundColor Green
 Start-Sleep -Milliseconds 500
-Set-Location $PSScriptRoot/Non-Lcapy-Files
+Set-Location $PSScriptRoot/NonLcapyFiles/tests
 
-
-pythonPath TestWithStandardCircuits.py
-if ($LASTEXITCODE -ne 0){
+C:\Users\yannick.wieland\AppData\Local\Programs\Python\lcapy1_24\Scripts\pytest.exe
+if ($LASTEXITCODE -ne 0)
+{
     Write-Host "An error occured while simplifiing the test circuits" -ForegroundColor Red
     Set-Location $startDir
     return
@@ -30,9 +34,12 @@ Set-Location $PSScriptRoot
 Write-Host "Building package" -ForegroundColor Green
 Start-Sleep -Milliseconds 1000
 
-pythonPath setup.py sdist bdist_wheel
-if ($LASTEXITCODE -ne 0){
+try{
+    pythonPath setup.py sdist bdist_wheel
+}
+catch{
     Write-Host "An error occured while building the package" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
     Set-Location $startDir
     return
 }
@@ -40,19 +47,19 @@ if ($LASTEXITCODE -ne 0){
 Set-Location $PSScriptRoot
 $version = pythonPath setup.py --version
 
-$content = Get-Content -Path "Non-Lcapy-Files\solve.py"
+$content = Get-Content -Path "NonLcapyFiles\solve.py"
 if($content[0][0] -eq "#"){
     $content[0] = [string]::Concat("# for lcapy version: ", $version)
-    Set-Content -Path "Non-Lcapy-Files\solve.py" -Value $content
+    Set-Content -Path "NonLcapyFiles\solve.py" -Value $content
 }
 else{
     $newContent = ,[string]::Concat("# for lcapy version: ", $version)
     $newContent += $content
     $content = $newContent
-    Set-Content -Path "Non-Lcapy-Files\solve.py" -Value $content
+    Set-Content -Path "NonLcapyFiles\solve.py" -Value $content
 }
 
-Set-Location $startDir
+Set-Location $PSScriptRoot
 Write-Host "Successfully tested and build package" -ForegroundColor Green
 Write-Host ([string]::Concat("Version is: ", $version)) -ForegroundColor Green
 
@@ -63,25 +70,41 @@ foreach($item in $oldPackages){
     Write-Output ([string]::Concat("Removed: ", $path))
 }
 
-Copy-Item -Path "Non-Lcapy-Files\solve.py" -Destination "..\Pyodide\"
-if ($LASTEXITCODE -ne 0){
+
+try {
+    Copy-Item -Path "NonLcapyFiles\solve.py" -Destination "..\Pyodide\"
+}
+catch{
     Write-Host "could not copy solve.py" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+    Set-Location $startDir
     return
 }
 Write-Output "Copied solve.py to: ..\Pyodide\"
 
-$newPackage = Get-ChildItem -Path "dist" -Filter "*.whl" -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
-if ($LASTEXITCODE -ne 0){
-    Write-Host "could find new package in .\dist" -ForegroundColor Red
+
+try{
+    $newPackage = Get-ChildItem -Path "dist" -Filter "*.whl" -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
+}
+catch {
+    Write-Host "couldn't find new package in .\dist" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+    Set-Location $startDir
     return
 }
 
-Copy-Item -Path ([string]::Concat("dist\", $newPackage)) -Destination "..\Pyodide\Packages\"
-if ($LASTEXITCODE -ne 0){
+
+try{
+    Copy-Item -Path ([string]::Concat("dist\", $newPackage)) -Destination "..\Pyodide\Packages\"
+}
+catch {
     Write-Host "could not copy new package" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+    Set-Location $startDir
     return
 }
 Write-Output ([string]::Concat("Copied ", $newPackage, " to: ..\Pyodide\Packages\"))
 
 
 Write-Host "Successfully updated solve.py and lcapy package in Pyodide distribution" -ForegroundColor Green
+Set-Location $startDir
