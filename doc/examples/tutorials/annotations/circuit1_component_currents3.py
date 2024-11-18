929243ab242ea7c921b1a073f70aa5645f938893
Michael Hayes <moikl.hayes@gmail.com>
2024-11-19 07:55:22 +1300

    Update annotations

--- doc/examples/tutorials/annotations/circuit1_component_currents3.py
+++ doc/examples/tutorials/annotations/circuit1_component_currents3.py
@@ -1,6 +1,4 @@
 from lcapy import *
 
 cct = Circuit('circuit1.sch')
-cct.annotate_currents(('R1', 'R2', 'R3', 'R4'), evalf=False, flow=True).draw(__file__.replace('.py', '.png'), draw_nodes='connections')
-
-
+cct.annotate_currents(('R1', 'R2', 'R3', 'R4'), style='eng3', flow=True).draw(__file__.replace('.py', '.png'), draw_nodes='connections')
