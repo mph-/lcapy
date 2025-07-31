8853f61a17e47edee296090183884d13b4e5d169
Michael Hayes <moikl.hayes@gmail.com>
2025-07-31 18:44:32 +1200

    Get __version__ without using deprecated pkg_resources

--- lcapy/__init__.py
+++ lcapy/__init__.py
@@ -73,14 +73,23 @@ from .exprclasses import *
 from .seqclasses import *
 
 import sys
-import pkg_resources
 del absolute_import, print_function
 
 
 name = "lcapy"
 
+from sys import version as python_version
+python_version = python_version.split(' ')[0]
+if python_version >= '3.8':
+    from importlib.metadata import version
+    __version__ = version('lcapy')
+    del version
+else:
+    import pkg_resources
+    __version__ = pkg_resources.require('lcapy')[0].version
 
-__version__ = pkg_resources.require('lcapy')[0].version
+del python_version
+
 lcapy_version = __version__
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
